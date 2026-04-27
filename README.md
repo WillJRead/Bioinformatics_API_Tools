3467abc7605292eca15975a003934299187c6369
WillJRead <williamjread26@outlook.com>
2026-04-27 13:41:45 +0100

    Updated functions, added to requirements

---
+++ README.md
@@ -0,0 +1,5 @@
+This project was created a personal development piece to improve my understanding of APIs in the context of genomic medicine. 
+
+Application Programming Interfaces (API) operate via a client sending a request to a server, and the server returning requested data. This is extremely useful in bioinformatics where there are multiple online databases that contain large amounts of genomic data that cannot simply be copied or downloaded.
+
+APIs operate of CRUD (Create, Return, Update, Delete) methods. Typically requests are sent using JSON which means that the server can handle the request despite what coding language the backend was built on. 
--- ensembl_API.py
+++ ensembl_API.py
@@ -1,20 +1,70 @@
-import requests 
-
-lookup = "lookup/id"
-
-def query_ensembl(variantID,method):
-    URL = f"https://rest.ensembl.org/{method}/{variantID}?"
-    response = requests.get(URL, headers={"Content-Type" : "application/json"})
+import requests, sys
+from pprint import pprint
 
 
+lookup = "lookup/id/"
+phenotype = "/phenotype/gene/human/"
+VEP = "vep/human/hgvs/"
 
-    if response.status_code == 200:
-        print(f"Succesfully retrieved data |--Code:{response.status_code}--|")
-        result_data = response.json()
-        
-        for k,v in result_data.items():
-            print(f"{k}:{v}")
-    else:
-        print(f"Failed to retrive data. |--Code:{response.status_code}--|")
+
+def query_ensembl(ext,on):
+    server = "http://rest.ensembl.org/"
+    URL = f"{server}{ext}{on}?"
     
-query_ensembl("ENST00000357654",lookup)
+    if ext == "vep/human/hgvs/":
+        new_URL = f"{URL}AlphaMissense=1&CADD=1&Conservation=1&REVEL=1&canonical=1&mane=1"
+        r2 = requests.get(new_URL, headers={"Content-Type":"application/json"})
+        if r2.status_code == 200:
+            print(f"---Request Successful---")
+        elif r2.status_code != 200:
+            print(f"---Request Failed---")
+            print(f"---Error {r.status_code}---")
+        response = r2.json()
+        print("---Filter by canonical transcript?---")
+        print("---y/n---")
+        answer = input()
+        filtered =[]
+        if answer == "y":
+            for i in response:
+                t = i.get("transcript_consequences")
+                for x in t:
+                    if x.get("canonical") == 1:
+                        filt = {k: v for k, v in i.items() if k !="transcript_consequences"}
+                        filtered.append(filt)
+                        filtered.append(x)
+            return filtered
+
+        else:
+             return response
+    else:
+        r = requests.get(URL, headers={"Content-type":"application/json"})
+        if r.status_code == 200:
+            print(f"---Request Successful---")
+        elif r.status_code != 200:
+            print(f"---Request Failed---")
+            print(f"---Error {r.status_code}---")
+    
+        response = r.json()
+        if ext == "/phenotype/gene/human/":
+            print("---Search via disease description?---")
+            print("---y/n---")
+            answer = input()
+            filtered =[]
+            if answer == "y":
+                    print("---Enter keyword---")
+                    keyword = input()
+                    for i in response:
+                        if keyword.upper() in i.get("description","").upper():
+                                filtered.append(i)
+                                pprint(i)
+                    return filtered
+            else: 
+                return response
+ 
+
+
+ 
+#gene = query_ensembl(lookup,"ENSG00000135100")
+#HPO = query_ensembl(phenotype,"ENSG00000135100")
+protein = query_ensembl(VEP,"ENST00000257555:c.544C>T")
+pprint(protein)
